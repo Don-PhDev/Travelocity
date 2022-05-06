80676f6256df608e04890ce5da760db7493a2d8d
Don Forrest <forrest@philippinedev.com>
2022-05-06 13:19:25 +0800

    Disable Webpacker and Yarn integrity check

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Disable Webpacker and Yarn integrity check
+  config.webpacker.check_yarn_integrity = false
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
